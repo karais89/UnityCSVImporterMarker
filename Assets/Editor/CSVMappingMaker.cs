8b56bea6326d945092383b77fa5d96e9c72e1aed
JH <karais89@gmail.com>
2018-01-02 00:32:05 +0900

    csv 파일 템플릿 파일에서 불러와서 자동으로 생성해주는 것 완료

--- Assets/Editor/CSVMappingMaker.cs
+++ Assets/Editor/CSVMappingMaker.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 
@@ -25,13 +26,15 @@ public class CSVMappingMaker : EditorWindow
         public string name;
     };
     
+    // todo : editorWindow에서 생성되는 경로 수정 할 수 있게
+    private static readonly string CLASS_CREATE_PATH = "Assets/Scripts/CSVData/"; // csvData 클래스가 생성되는 곳
     private static readonly string KEY_PREFS = "CSVMappingMaker_"; // 윈도우 정보 임시 저장
     private string _filePath; // csv 파일 경로
     private string _fileName; // csv 파일 이름
     private string _className; // csv 파일을 매핑할 클래스 이름
     private List<RowParam> _rowParams = new List<RowParam>();
     private Vector2 _scrollPosition;
-    
+
     [MenuItem("Assets/CSV Mapping To Class")]
     public static void CSVMappingToClass()
     {
@@ -42,18 +45,24 @@ public class CSVMappingMaker : EditorWindow
             {
                 continue;
             }
-            
+
             // 무조건 새로운 윈도우를 생성 해준다.
             var window = CreateInstance<CSVMappingMaker>();
             window._filePath = AssetDatabase.GetAssetPath(obj);
             window._fileName = Path.GetFileNameWithoutExtension(window._filePath);
-            
+
+            if (Path.GetExtension(window._filePath).ToLower() != ".csv")
+            {
+                Debug.LogError("This is file not csv format");
+                continue;
+            }
+
             // 해당 파일 경로의 텍스트를 읽어온다.
             string csv = File.ReadAllText(window._filePath);
-            
+
             // 해당 파일이 있으면 이전 정보를 세팅 해주고, 없으면 기본 값을 세팅 해준다.
             window._className = EditorPrefs.GetString(GetKeyPrefsByClassName(window), window._fileName + "CSVData");
-            
+
             // csv 파싱
             var data = SimpleCSVParser.Parser(csv);
             if (data.Count < 2)
@@ -61,27 +70,28 @@ public class CSVMappingMaker : EditorWindow
                 Debug.LogError("최소한 헤더 정보와 값 정보를 가지고 있어야 합니다. (row가 2줄 이상)");
                 continue;
             }
-            
+
             var headerMeta = data[0]; // 자료형 이름 판단
             var rowMeta = data[1]; // 자료형 타입 판단
             window._rowParams.Clear();
             for (int i = 0; i < headerMeta.Count; i++)
             {
                 RowParam param = new RowParam();
-         
+
                 param.name = headerMeta[i];
 
                 if (EditorPrefs.HasKey(GetKeyPrefsByType(window, param)))
                 {
-                    param.type = (ValueType)EditorPrefs.GetInt(GetKeyPrefsByType(window, param));
+                    param.type = (ValueType) EditorPrefs.GetInt(GetKeyPrefsByType(window, param));
                 }
                 else
                 {
                     param.type = GetValueTypeByRowMeta(rowMeta[i]);
                 }
-                
+
                 window._rowParams.Add(param);
             }
+
             window.Show();
         }
     }
@@ -108,42 +118,42 @@ public class CSVMappingMaker : EditorWindow
         int intValue;
         float floatValue;
         bool boolValue;
-        
+
         if (int.TryParse(rowData, out intValue))
         {
             return ValueType.Int;
         }
-        
+
         if (float.TryParse(rowData, out floatValue))
         {
             return ValueType.Float;
         }
-        
+
         if (Boolean.TryParse(rowData, out boolValue))
         {
             return ValueType.Bool;
         }
-        
+
         return ValueType.String;
     }
 
     private void OnGUI()
     {
         var window = GetWindow<CSVMappingMaker>();
-        
+
         GUILayout.Label("CSV Mapping To Class", EditorStyles.boldLabel);
         _className = EditorGUILayout.TextField("class Name", _className);
-        
+
         if (GUILayout.Button("Csv Data Mapping!!"))
         {
             EditorPrefs.SetString(GetKeyPrefsByClassName(window), _className);
-            // todo : 파일 생성 및 에셋 업데이트
-            
+            // 파일 생성 및 에셋 업데이트
+            CreateFileCsvMapping();
             AssetDatabase.ImportAsset(_filePath);
             AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
             Close();
         }
-        
+
         // csv header settings
         EditorGUILayout.LabelField("csv header settings");
         _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
@@ -155,12 +165,76 @@ public class CSVMappingMaker : EditorWindow
 
             row.name = EditorGUILayout.TextField(row.name);
             row.type = (ValueType) EditorGUILayout.EnumPopup(row.type, GUILayout.MaxWidth(100));
-            EditorPrefs.SetInt(GetKeyPrefsByType(window, row), (int)row.type);
-            
+            EditorPrefs.SetInt(GetKeyPrefsByType(window, row), (int) row.type);
+
             GUILayout.EndHorizontal();
         }
-        
+
         EditorGUILayout.EndVertical();
         EditorGUILayout.EndScrollView();
+    }
+
+    private void CreateFileCsvMapping()
+    {
+        // template 파일 위치 변경시 변경 해줘야 됨.
+        string templateFilePath = "Assets/Editor/MappingTemplate.txt";
+        string templateStr = File.ReadAllText(templateFilePath);
+
+        templateStr = templateStr.Replace("$CLASS_NAME$", _className);
+        templateStr = templateStr.Replace("$TYPES$", GetTypesBuilder().ToString());
+        templateStr = templateStr.Replace("$EXPORT_DATA$", GetExportDataBuilder().ToString());
+
+        // todo : 파일 생성
+        Directory.CreateDirectory(CLASS_CREATE_PATH);
+        File.WriteAllText(CLASS_CREATE_PATH + _className + ".cs", templateStr);
+    }
+
+    private StringBuilder GetTypesBuilder()
+    {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < _rowParams.Count; i++)
+        {
+            if (i != 0)
+            {
+                builder.AppendLine();
+            }
+
+            var row = _rowParams[i];
+            builder.AppendFormat("        public {0} {1} {{ get; set; }}",
+                row.type.ToString().ToLower(),
+                row.name);
+        }
+        
+        return builder;
+    }
+
+    private StringBuilder GetExportDataBuilder()
+    {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < _rowParams.Count; i++)
+        {
+            if (i != 0)
+            {
+                builder.AppendLine();
+            }
+
+            string formatStr = string.Format("parserData[i][{0}]", i);
+            switch (_rowParams[i].type)
+            {
+                case ValueType.Bool:
+                case ValueType.Int:
+                case ValueType.Float:
+                case ValueType.Double:
+                    formatStr = string.Format("{0}.Parse({1})", 
+                        _rowParams[i].type.ToString().ToLower(), 
+                        formatStr);
+                    break;
+                default:
+                    break;
+            }
+            builder.AppendFormat("            row.{0} = {1};", _rowParams[i].name, formatStr);
+        }
+
+        return builder;
     }
 }
